fdec9cde2b89f0717685970ac422034ba1786e12
Coder2 <coder2@corp.com>
2021-11-24 12:09:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args){
+        MyArrayList arr = new MyArrayList();
+        arr.add('a');
+        arr.add('b');
+        arr.add('c');
+        arr.add('d');
+        System.out.println(arr);
+    }
+}
